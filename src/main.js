9483e61b3fbceb98714bf09056b58d95e8d369ff
Matheus de Souza <matheusdesouza187@gmail.com>
2022-12-24 17:32:06 -0300

    feat: Instalação do Bootstrap na versão 5.2 no projeto

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+
 createApp(App).use(router).mount('#app')
+
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about pagegg</h1>
+    <button type="button" class="btn btn-primary">
+  Notifications <span class="badge text-bg-secondary">4</span>
+</button>
   </div>
 </template>

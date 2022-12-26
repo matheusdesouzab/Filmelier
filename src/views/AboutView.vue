a4ead248213a487a8c5b2bb41729390ba9526465
Matheus de Souza <matheusdesouza187@gmail.com>
2022-12-26 10:09:19 -0300

    fix: Remoção do componente 'AboutView'

--- src/views/AboutView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about pagegg</h1>
-    <button type="button" class="btn btn-primary">
-  Notifications <span class="badge text-bg-secondary">4</span>
-</button>
-  </div>
-</template>
